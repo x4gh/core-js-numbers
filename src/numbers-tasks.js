6ea4e39fd68ef5fcceb685946e9ecbd1da9c0931
x4gh <v.asepashvili@gmail.com>
2025-09-19 02:48:31 +0300

    feat: add solutiond for toFixed and toPrecision

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -370,8 +370,8 @@ function toExponential(number, fractionDigits) {
  * 12345, 2    => '12345.00'
  * 12.345, 1   => '12.3'
  */
-function toFixed(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toFixed(number, fractionDigits) {
+  return number.toFixed(fractionDigits);
 }
 
 /**
@@ -386,8 +386,8 @@ function toFixed(/* number, fractionDigits */) {
  * 12345, 7    => '12345.00'
  * 12.345, 4   => '12.35'
  */
-function toPrecision(/* number, precision */) {
-  throw new Error('Not implemented');
+function toPrecision(number, precision) {
+  return number.toPrecision(precision);
 }
 
 /**
